570e8b578a60a025216ca472edf2269588619612
Nima Hejazi <nh@nimahejazi.org>
2018-02-14 12:25:14 -0800

    mostly finalize

--- install.R
+++ install.R
@@ -1,4 +1,8 @@
 install.packages("tidyverse")
 install.packages("data.table")
-install.packages("ggsci")
-install.packages("origami")
+install.packages("remotes")
+install.packages("rlang")
+remotes::install_github("osofr/condensier@master")
+remotes::install_github("jeremyrcoyle/origami@master")
+remotes::install_github("jeremyrcoyle/sl3@master")
+
