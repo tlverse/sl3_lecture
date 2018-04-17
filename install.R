9b47002f75ded077a723a6a0895949401c46339b
Nima Hejazi <nh@nimahejazi.org>
2018-04-17 16:57:50 -0700

    install devel version of sl3

--- install.R
+++ install.R
@@ -6,5 +6,5 @@ install.packages("rlang")
 install.packages("SuperLearner")
 remotes::install_github("osofr/condensier@master")
 remotes::install_github("tlverse/origami@master")
-remotes::install_github("tlverse/sl3@master")
+remotes::install_github("tlverse/sl3@devel")
 
